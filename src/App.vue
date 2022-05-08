be96d548e4856892d764b01d635e2fd709e02610
ibraheemsulay <ibraheemsulay@gmail.com>
2022-05-08 18:13:19 +0100

    added loading stating

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,33 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import { ref } from "vue";
+  import img from "./assets/images";
+
+  const { Logo } = img;
+  const loader = ref(true);
+  setTimeout(() => (loader.value = !loader.value), 3000);
+</script>
 
 <template>
-  <Layout />
+  <div
+    v-if="loader"
+    class="tw-h-screen tw-w-screen tw-flex tw-justify-center tw-items-center"
+  >
+    <img class="loader tw-w-20" :src="Logo" alt="apex logo" />
+  </div>
+  <Layout v-else />
 </template>
 
-<style></style>
+<style>
+  .loader {
+    animation: spinner 1s 0.1s infinite;
+  }
+
+  @keyframes spinner {
+    from {
+      transform: rotateZ(0deg);
+    }
+    to {
+      transform: rotateZ(360deg);
+    }
+  }
+</style>
