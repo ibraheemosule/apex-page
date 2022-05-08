66c4251a54c09ec522212d95208f1f1562072d1d
ibraheemsulay <ibraheemsulay@gmail.com>
2022-05-08 19:40:42 +0100

    added gsap animation for scrolling effect and change the package installer to yarn

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -9,7 +9,29 @@
   import GrowBusiness from "./collections/GrowBusiness.vue";
   import FooterNav from "./collections/FooterNav.vue";
   import Copyright from "./collections/Copyright.vue";
-  import { provide } from "vue";
+  import { onMounted, provide } from "vue";
+  import { gsap } from "gsap";
+  import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+  gsap.registerPlugin(ScrollTrigger);
+
+  onMounted(() => {
+    let elements = document.getElementsByTagName("section");
+    for (let el of Array.from(elements).slice(1)) {
+      gsap
+        .timeline({
+          scrollTrigger: {
+            trigger: el,
+            toggleActions: "restart reverse restart reset",
+          },
+        })
+        .fromTo(
+          el,
+          { scale: 0.95, autoAlpha: 0 },
+          { duration: 0.75, scale: 1, autoAlpha: 1 }
+        );
+    }
+  });
 
   provide("homeUrl", "https://apexnetwork.co/");
 </script>
@@ -19,28 +41,29 @@
     <section class="md:tw-container">
       <Navbar />
     </section>
-    <section class="tw-container">
+    <section class="tw-container anim-1">
       <Hero />
+
+      <div class="tw--mt-40 tw-z-30 tw-py-16 tw-bg-white anim-2">
+        <Features />
+      </div>
     </section>
-    <section class="tw-container tw--mt-40 tw-z-30 tw-py-16 tw-bg-white">
-      <Features />
-    </section>
-    <section class="tw-bg-bg-3 tw-py-8">
+    <section class="tw-bg-bg-3 tw-py-8 anim-3">
       <MeetCustomers />
     </section>
-    <section class="tw-py-8 tw-overflow-hidden">
+    <section class="tw-py-8 tw-overflow-hidden anim-4">
       <StartSelling />
     </section>
-    <section class="tw-bg-bg-3 tw-pt-8 tw-pb-24 md:tw-pb-32">
+    <section class="tw-bg-bg-3 tw-pt-8 tw-pb-24 md:tw-pb-32 anim-5">
       <Orders />
     </section>
-    <section class="tw-bg-[#FB8E0B]">
+    <section class="tw-bg-[#FB8E0B] anim-6">
       <AboutUs />
     </section>
-    <section class="tw-container">
+    <section class="tw-container anim-7">
       <GrowBusiness />
     </section>
-    <footer class="tw-container">
+    <footer class="tw-container anim-8">
       <FooterNav />
       <Copyright />
     </footer>
